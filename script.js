a5efe07c7d44238d8142d62d1a35d3e56a959681
Ravi Meena <97457969+skykingravi@users.noreply.github.com>
2023-05-20 18:38:14 +0530

    uploading threejs-template

---
+++ script.js
@@ -0,0 +1,87 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+// Window Resizing
+window.addEventListener("resize", () => {
+    // Update Sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update Camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update Renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+camera.position.set(1, 1, 3);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+controls.minDistance = 1;
+controls.maxDistance = 10;
+
+// Cube
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xe6447d })
+);
+scene.add(cube);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Animate
+
+// Create a clock
+const clock = new THREE.Clock();
+
+// Create a function that will be called every frame
+function tick() {
+    // Get the elapsed time since the last frame
+    const elapsedTime = clock.getElapsedTime();
+
+    // Update your objects or perform any animations here based on the elapsed time
+
+    // Update controls
+    controls.update();
+
+    // Render the scene
+    renderer.render(scene, camera);
+
+    // Call the tick function again on the next frame
+    requestAnimationFrame(tick);
+}
+
+// Call the tick function for the first time to start the animation loop
+tick();
